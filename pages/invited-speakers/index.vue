99374d96ea6a2bdddca9f3a9e5acd11d2edd59d4
youjiayou <look53486@gmail.com>
2025-04-29 09:26:58 +0800

    Add speaker page

---
+++ pages/invited-speakers/index.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <main class="common-section">
+            <Banner></Banner>
+            <!-- <h1 class="title">Invited Speakers</h1> -->
+            <Title title="Invited Speakers"></Title>
+            <Breadcrumbs firstRoute="Program" secoundRoute="Invited Speakers"></Breadcrumbs>
+            <div class="content">
+                <speaker class="speaker" v-for="item in speakers" :speaker="item"></speaker>
+            </div>
+        </main>
+
+    </div>
+</template>
+<script setup lang="ts">
+import Breadcrumbs from '@/components/layout/Breadcrumbs.vue'
+import Banner from '@/components/layout/Banner.vue';
+import Speaker from './components/speaker.vue';
+import Title from '@/components/layout/Title.vue';
+
+
+const speakers = reactive<any>([]);
+
+const getSpeakers = async () => {
+  let res = await CSRrequest.get('/invited-speaker/pagination', {
+    params: {
+      page: 1,
+      size: 15,
+    },
+  })
+    console.log(res);
+  if (res.code === 200) {
+    Object.assign(speakers, res.data.records);
+  }
+};
+
+onMounted(() => {
+  getSpeakers();
+});
+
+
+
+</script>
+<style lang="scss" scoped>
+.common-section {
+    // width: $common-section-width;
+    margin: $common-section-margin;
+    font-family: $common-section-font-family;
+
+    .title {
+        margin-left: 7.4%;
+        font-size: 2.5rem;
+        color: $main-color;
+    }
+    .content {
+        width: 80%;
+        margin: 1rem auto;
+        text-align: center;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+        gap: 2rem;
+        padding: 2rem;
+        h1 {
+            font-size: 1.5rem;
+        }
+
+        .speaker {
+          width: calc(100% / 5 - 2rem);
+
+          @media screen and (max-width: 1200px) {
+            width: calc(100% / 3 - 2rem);
+          }
+          @media screen and (max-width: 800px) {
+            width: calc(100% / 2 - 2rem);
+          }
+          @media screen and (max-width: 500px) {
+            width: calc(100% - 2rem);
+          }
+        }
+    }
+}
+</style>
